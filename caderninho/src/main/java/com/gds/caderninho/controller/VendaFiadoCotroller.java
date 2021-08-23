710bd7d2663923df73bd3c60a1d0d0ea4a3ba823
Geoguitar <geovanedsilveira@gmail.com>
2021-08-23 16:07:12 -0300

    realizado requisições para view

--- caderninho/src/main/java/com/gds/caderninho/controller/VendaFiadoCotroller.java
+++ caderninho/src/main/java/com/gds/caderninho/controller/VendaFiadoCotroller.java
@@ -1,7 +1,12 @@
 package com.gds.caderninho.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class VendaFiadoCotroller {
 
+		@RequestMapping
 		public String novo() {
 			return "CadastroVendaFiado";
 		}
