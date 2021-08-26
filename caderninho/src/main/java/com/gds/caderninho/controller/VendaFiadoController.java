91464dd6ffba626e297dd9a71e918836b937e238
Geoguitar <geovanedsilveira@gmail.com>
2021-08-26 01:55:13 -0300

    criado metodo salvar para BD

--- caderninho/src/main/java/com/gds/caderninho/controller/VendaFiadoController.java
+++ caderninho/src/main/java/com/gds/caderninho/controller/VendaFiadoController.java
@@ -2,13 +2,23 @@ package com.gds.caderninho.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.gds.caderninho.model.VendaFiado;
 
 @Controller
+@RequestMapping("/vendaFiado")
 public class VendaFiadoController {
 
-		@RequestMapping ("/vendaFiado/novo")
+		@RequestMapping ("/novo")
 		public String novo() {
 			return "CadastroVendaFiado";
 		}
-	
+
+		//Salvando no BD
+		
+		@RequestMapping(method = RequestMethod.POST)
+		public void salvar(VendaFiado vendaFiados) { 
+			
+		}
 }
